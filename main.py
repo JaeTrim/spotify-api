b27a01d5cb8379eff553c03a169cb98cf1b80d6e
Jae Trimboli <jaetrim@vt.edu>
2023-07-17 17:11:04 -0400

    update main with ttkboostrap

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import pandas
-import requests
-
 from dotenv import load_dotenv
+
 from tkinter import *
+from tkinter.ttk import *
+from ttkbootstrap import Style
+
+import requests
 
 import os
 import base64
@@ -15,25 +17,18 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
 
-# Creates Spotify API Auth Token
+# Creates Spotify API Auth Token; encodes formatted string containing client id and client secret into bytes and then
+# converts binary data to text. Uses GET request to obtain Spotify data
 def create_token(c_id, c_secret):
-    # Formats token by combining client id and secret
     format_token = c_id + ":" + c_secret
-    # Uses encode function to encode the string into bytes
     token_bytes = format_token.encode("utf-8")
-    # Applies Base64 encoding to the token_bytes, converting the binary data into text
     bin_to_text = str(base64.b64encode(token_bytes), "utf-8")
-
-    # Headers, Data, and URL required for POST request
     headers = {
         "Authorization": "Basic " + bin_to_text,
         "Content-Type": "application/x-www-form-urlencoded"
     }
-
     data = {"grant_type": "client_credentials"}
-
     spotify_url = "https://accounts.spotify.com/api/token"
-
     response = requests.post(url=spotify_url, headers=headers, data=data)
     json_data = response.json()
     access_token = json_data["access_token"]
@@ -55,10 +50,10 @@ def create_list(artist_type):
 # Mode 1 == Save Song Names
 # Mode 2 == Display related Artists
 # Makes global variable for the mode so the program knows which information to display based on mode
+# Based on the which mode, the screen displays different prompts
 selected_mode = -1
 
 
-# Based on the which mode, the screen displays different prompts
 def mode():
     global selected_mode
     selected_mode += 1
@@ -81,25 +76,33 @@ def mode():
         sub_header("Enter Song Names to Keep Track Of")
 
 
+# Resets the canvas text to the proper mode
+def reset_canvas(text):
+    canvas.delete("all")
+    main_header()
+    sub_header(f"{text}")
+
+
+# Deals with the main portion of the project, finding artists and
+# songs and displaying on the screen based on selected mode
 def spotify_data():
-    # Finds spotify artist as well as their top songs and displays them into a csv file
     height = 275
     if selected_mode == 0:
-        # User inputs artist in entry box and the name is received
+        reset_canvas("Enter an Artist to See Popular Songs")
         user_input = user_entry.get()
-        # Calls the spotify client and finds the artist based on the user input
         artist_data = get_spot_api.find_artist(user_input)
         art_id = artist_data["id"]
         songs = get_spot_api.get_artist_songs(art_id)
         song_list = create_list(songs)
         count = 0
         while count < 10:
-            songs = canvas.create_text(400, height, text=f"{song_list[count]}", font=("Helvetica", 20, "bold"),
+            songs = canvas.create_text(400, height, text=f"{song_list[count]}", font=("Helvetica", 16, "normal"),
                                        fill="white")
             count += 1
-            height += 30
+            height += 28
     # Show related artists
     elif selected_mode == 1:
+        reset_canvas("Enter artist to see related artists")
         user_input = user_entry.get()
         artist_data = get_spot_api.find_artist(user_input)
         art_id = artist_data["id"]
@@ -107,19 +110,23 @@ def spotify_data():
         artist_list = create_list(related_artists)
         count = 0
         while count < 10:
-            related_list = canvas.create_text(400, height, text=f"{artist_list[count]}", font=("Helvetica", 20, "bold"),
+            related_list = canvas.create_text(400, height, text=f"{artist_list[count]}",
+                                              font=("Helvetica", 16, "normal"),
                                               fill="white")
             count += 1
-            height += 30
+            height += 28
     else:
-        pass
+        reset_canvas("Enter Song Names to Keep Track Of")
         # Code this to eventually create a spotify playlist that contains the songs that have been entered
 
 
 # Creates Tkinter GUI Interface
 window = Tk()
 window.title("Spotify Stats")
-window.config(padx=50, pady=50, bg="#1DB954")
+window.geometry("900x700")
+window.config(padx=50, pady=50)
+
+style = Style(theme="darkly")
 
 canvas = Canvas(width=800, height=600)
 canvas.grid(row=0, column=0, columnspan=4, rowspan=4)
@@ -131,7 +138,7 @@ def main_header():
     canvas.create_text(400,
                        150,
                        text="Spotify Tracker",
-                       font=("Helvetica", 40, "bold"),
+                       font=("Ariel", 40, "bold"),
                        fill="white")
 
 
@@ -140,7 +147,7 @@ def sub_header(text_entry):
     canvas.create_text(400,
                        190,
                        text=f"{text_entry} ",
-                       font=("Helvetica", 20, "bold"),
+                       font=("Trebuchet MS", 20, "bold"),
                        fill="white")
 
 
@@ -149,17 +156,13 @@ main_header()
 sub_header("Press Mode to Begin")
 
 # Creates entry widget
-user_entry = Entry(width=30, font=("Ariel", 15, "bold"), justify=CENTER)
+user_entry = Entry(width=30, font=("Ariel", 15, "bold"), justify=CENTER, bootstyle="success")
 user_entry.grid(row=1, column=0, columnspan=4)
 
-# Enter button to display data
-artist_submit_button = Button(text="ENTER", highlightthickness=0, command=spotify_data, justify=CENTER)
-
-# Mode button to change mode
-mode_button = Button(text="MODE", highlightthickness=0, command=mode)
-
-# Adds buttons to the screen
-mode_button.grid(row=1, column=0, columnspan=2)
+# Mode and Enter buttons
+artist_submit_button = Button(text="Enter", command=spotify_data, bootstyle="success")
 artist_submit_button.grid(row=1, column=2, columnspan=2)
+mode_button = Button(text="Mode", command=mode, bootstyle="success")
+mode_button.grid(row=1, column=0, columnspan=2)
 
 window.mainloop()
